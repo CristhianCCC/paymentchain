da0b8cf2e2250d66dee4d7e69a2e2c16900c9aa9
CristhianCCC <cristhiancamilocalero10@gmail.com>
2025-04-26 01:08:19 -0500

    añadiendo Netflix Eureka y actuator, configuracion no esta terminada aun

---
+++ paymentchainparent/infraestructuredomain/eurekaserver/src/main/java/com/paymentchain/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.paymentchain.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+//anotacion para poder correr eurekaserver
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
